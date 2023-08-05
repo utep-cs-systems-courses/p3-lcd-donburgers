5238cfc26064b23d853e5c8188f4f948188d240d
Brian Mata <37246371+donburgers@users.noreply.github.com>
2023-08-05 00:49:29 -0600

    Melody working, separted buzzer and button functions

--- projectMain/main.c
+++ projectMain/main.c
@@ -1,26 +1,34 @@
 #include <msp430.h>
+#include "notes.h"
+#include "button.h"
 
-// Define the frequencies for musical notes
-#define NOTE_C4  261.63
-#define NOTE_D4  293.66
-#define NOTE_E4  329.63
-#define NOTE_F4  349.23
-#define NOTE_G4  392.00
-#define NOTE_A4  440.00
-#define NOTE_B4  493.88
 
 // Define the frequency for the system clock (SMCLK)
 #define SMCLK_FREQ 1000000  // Assuming SMCLK is set to 1MHz
 
-// Define the melody sequence
 float melody[] = {
-    NOTE_C4, NOTE_C4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_G4
+    NOTE_E6, NOTE_D6, NOTE_A5,  
+    NOTE_E6, NOTE_D6, NOTE_G6, NOTE_F6, NOTE_E6, //Measure 2
+    NOTE_A6, NOTE_E6, // Measure 3
+    NOTE_E6, NOTE_F6, NOTE_G6, NOTE_E6, 
+    NOTE_E6, NOTE_D6, NOTE_C6, NOTE_B5, 
+    NOTE_B5, NOTE_C6, NOTE_D6, NOTE_B5,
 };
 
 // Define the duration for each note in the melody
 int noteDurations[] = {
-    4, 4, 4, 4, 4, 4, 2  // 4 represents a quarter note, 2 represents a half note, etc.
+  //1 = whole note
+  //2 = Half notes
+    2, 2, 1, // 1st Measure 
+    2, 2, 1, 2, 1, // 2nd Measure 
+    2, 1, 
+    2, 2, 2, 1, 
+    2, 2, 2, 1, 
+    2, 2, 2, 1, 
+    
+    
 };
+
 
 // Set up Timer A to run in up mode
 void timerAUpmode()
@@ -42,13 +50,22 @@ void buzzer_init()
 }
 
 // Play a note on the buzzer
-void playBuzzer(float frequency)
+void playBuzzer(float frequency, int duration)
 {
     int timerPeriod = SMCLK_FREQ / frequency;  // Calculate timer period for desired frequency
     TA0CCR0 = timerPeriod;
     TA0CCR1 = timerPeriod / 2;  // 50% duty cycle
     TA0CTL |= TASSEL_2 + MC_1;  // Source: SMCLK, Mode: Up to CCR0
-    P1OUT |= (BIT0 | BIT1);  // Turn on LEDs on P1.0 and P1.1
+
+    // Blink LEDs based on duration
+    if (duration == 1) {  // If it's a whole note
+        P1OUT |= BIT0;  // Turn on LED on P1.0
+        P1OUT &= ~BIT2;  // Ensure LED on P1.2 is off
+    }
+    else if (duration == 2) {  // If it's a half note
+        P1OUT |= BIT2;  // Turn on LED on P1.2
+        P1OUT &= ~BIT0;  // Ensure LED on P1.0 is off
+    }
 }
 
 // Stop the buzzer
@@ -56,8 +73,9 @@ void stopBuzzer()
 {
     TA0CTL = MC_0;  // Stop the timer
     P2OUT &= ~BIT6;  // Turn off the buzzer
-    P1OUT &= ~(BIT0 | BIT1);  // Turn off LEDs on P1.0 and P1.1
+    P1OUT &= ~(BIT0 | BIT2);  // Turn off LEDs on P1.0 and P1.2
 }
+
 
 // Delay for a specified number of milliseconds
 void delay_ms(int ms)
@@ -74,13 +92,19 @@ int main(void)
     WDTCTL = WDTPW + WDTHOLD;  // Stop watchdog timer
 
     buzzer_init();  // Initialize the buzzer
+    button_init();  // Initialize the button
+    
+    P1DIR |= (BIT0 | BIT2);  // Set P1.0 and P1.2 as outputs
+    P1OUT &= ~(BIT0 | BIT2);  // Ensure LEDs are off to start
 
-    P1DIR |= (BIT0 | BIT1);  // Set P1.0 and P1.1 as outputs
-    P1OUT &= ~(BIT0 | BIT1);  // Ensure LEDs are off to start
+    // Wait for the button to be pressed
+    while ((P2IN & BIT0) == BIT0) {  // while button is not pressed
+        // do nothing
+    }
 
-    // Play the melody
+    // Once button is pressed, play the melody
     for (int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++) {
-        playBuzzer(melody[i]);
+        playBuzzer(melody[i], noteDurations[i]);
         delay_ms(1000 / noteDurations[i]);
         stopBuzzer();
         delay_ms(250 / noteDurations[i]);  // Short delay between notes
